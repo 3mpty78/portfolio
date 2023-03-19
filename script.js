85af15b13c256fc43ea677d58c6e24d49186baad
3mpty <aurelien.corre@outlook.fr>
2023-03-19 21:20:03 +0100

    contact section design done

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const slider = document.querySelector(".slider");
 const leftArrow = document.querySelector(".left");
 const rightArrow = document.querySelector(".right");
+const carousel = document.querySelector(".contact");
+const previous = document.querySelector(".prev");
+const next = document.querySelector(".next");
 
 let index = 0;
 
@@ -11,4 +14,13 @@ leftArrow.addEventListener("click", () => {
 rightArrow.addEventListener("click", () => {
   index = index < 3 ? index + 1 : 0;
   slider.style.transform = "translate(" + index * -25 + "%)";
+});
+
+previous.addEventListener("click", () => {
+  index = index > 0 ? index - 1 : 3;
+  carousel.style.transform = "translateY(" + index * -25 + "%)";
+});
+next.addEventListener("click", () => {
+  index = index < 3 ? index + 1 : 0;
+  carousel.style.transform = "translateY(" + index * -25 + "%)";
 });
