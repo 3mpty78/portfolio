1fe6b48dcbbb1b5aea0c85eaf42707b1820b597a
3mpty <aurelien.corre@outlook.fr>
2023-06-01 22:41:33 +0200

    Updated projects section

--- script.js
+++ script.js
@@ -13,39 +13,44 @@ window.addEventListener("mousemove", moveCursor);
 
 // Sliders script
 
-const slider = document.querySelector(".slider");
-const leftArrow = document.querySelector(".left");
-const rightArrow = document.querySelector(".right");
-
 const carousel = document.querySelector(".contact");
 const previous = document.querySelector(".prev");
 const next = document.querySelector(".next");
 
-const body = document.querySelector(".slide");
+const firstProject = document.querySelector(".first_project");
+const secondProject = document.querySelector(".second_project");
+const thirdProject = document.querySelector(".third_project");
+const slides = document.querySelector(".mockup_picture_container");
+const textSlides = document.querySelector(".mockup_description_container");
 
 let index = 0;
-
-leftArrow.addEventListener("click", () => {
-  index = index > 0 ? index - 1 : 3;
-  slider.style.transform = "translate(" + index * -25 + "%)";
-});
-rightArrow.addEventListener("click", () => {
-  index = index < 3 ? index + 1 : 0;
-  slider.style.transform = "translate(" + index * -25 + "%)";
-});
 
 previous.addEventListener("click", () => {
   index = index > 0 ? index - 1 : 3;
   carousel.style.transform = "translateY(" + index * -25 + "%)";
 });
-next.addEventListener("onClick", () => {
+next.addEventListener("click", () => {
   index = index < 3 ? index + 1 : 0;
   carousel.style.transform = "translateY(" + index * -25 + "%)";
 });
-body.addEventListener("onClick", () => {
-  console.log("ça click");
-});
 
+firstProject.addEventListener("click", () => {
+  index = index > 0 ? index - 1 : 2;
+  slides.style.transform = "translate(" + index * -33.33 + "%)";
+  textSlides.style.transform = "translate(" + index * -33.33 + "%)";
+});
+secondProject.addEventListener("click", () => {
+  if (index > 0 && index < 2) {
+    index = 2;
+    slides.style.transform = "translate(" + index * -33.33 + "%)";
+    textSlides.style.transform = "translate(" + index * -33.33 + "%)";
+  }
+});
+thirdProject.addEventListener("click", () => {
+  index = index > 0 ? index - 1 : 2;
+  slides.style.transform = "translate(" + index * -33.33 + "%)";
+  textSlides.style.transform = "translate(" + index * -33.33 + "%)";
+});
 // Style for overflow
 
-oDomElement.style.overflowY = "hidden";
+// oDomElement.style.overflowY = "hidden";
